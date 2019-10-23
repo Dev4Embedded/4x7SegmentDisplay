725be0e9b04c2875d841087c33199847b158d090
Dev4Embedded <integralzerox@gmail.com>
2019-10-23 20:00:47 +0200

    code cleaning and alignment: 4SegmentsDisplay.vhd

--- LedDisplay.srcs/sources_1/new/4SegmentsDisplay.vhd
+++ LedDisplay.srcs/sources_1/new/4SegmentsDisplay.vhd
@@ -32,47 +32,49 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity FourSegmentsDisplay is
-    Port ( word : in STD_LOGIC_VECTOR (15 downto 0);
-           display : out STD_LOGIC_VECTOR (6 downto 0);
-           segments : out STD_LOGIC_VECTOR (3 downto 0);
-           clk : in STD_LOGIC);
+	Port (
+			display  : out STD_LOGIC_VECTOR (6 downto 0);
+			segments : out STD_LOGIC_VECTOR (3 downto 0);
+			rx       : in STD_LOGIC;
+			clk      : in STD_LOGIC
+		);
 end FourSegmentsDisplay;
 
 architecture behavior of FourSegmentsDisplay is
 
 component led_segment
-    Port (
-        hex  : in STD_LOGIC_VECTOR (3 downto 0);
-        leds : out STD_LOGIC_VECTOR (6 downto 0)
-        );
+	Port (
+		hex  : in STD_LOGIC_VECTOR (3 downto 0);
+		leds : out STD_LOGIC_VECTOR (6 downto 0)
+		);
 end component;
 
 component demux
-    Port (
-        in_A : in STD_LOGIC_VECTOR (3 downto 0);
-        in_B : in STD_LOGIC_VECTOR (3 downto 0);
-        in_C : in STD_LOGIC_VECTOR (3 downto 0);
-        in_D : in STD_LOGIC_VECTOR (3 downto 0);
-        out_sel : out STD_LOGIC_VECTOR (3 downto 0);
-        set : in STD_LOGIC_VECTOR (1 downto 0)
-    );
+	Port (
+		in_A    : in STD_LOGIC_VECTOR (3 downto 0);
+		in_B    : in STD_LOGIC_VECTOR (3 downto 0);
+		in_C    : in STD_LOGIC_VECTOR (3 downto 0);
+		in_D    : in STDW_LOGIC_VECTOR (3 downto 0);
+		out_sel : out STD_LOGIC_VECTOR (3 downto 0);
+		set     : in STD_LOGIC_VECTOR (1 downto 0)
+	);
 end component;
 
 component word2hex
-    Port ( 
-          word : in STD_LOGIC_VECTOR (15 downto 0);
-          nibble0 : out STD_LOGIC_VECTOR (3 downto 0);
-          nibble1 : out STD_LOGIC_VECTOR (3 downto 0);
-          nibble2 : out STD_LOGIC_VECTOR (3 downto 0);
-          nibble3 : out STD_LOGIC_VECTOR (3 downto 0)
-     );
+	Port ( 
+		word    : in STD_LOGIC_VECTOR (15 downto 0);
+		nibble0 : out STD_LOGIC_VECTOR (3 downto 0);
+		nibble1 : out STD_LOGIC_VECTOR (3 downto 0);
+		nibble2 : out STD_LOGIC_VECTOR (3 downto 0);
+		nibble3 : out STD_LOGIC_VECTOR (3 downto 0)
+	);
 end component;
 
 component segment_driver
-    Port ( clk : in STD_LOGIC;
-           selector : out STD_LOGIC_VECTOR (1 downto 0);
-           segment : out STD_LOGIC_VECTOR (3 downto 0)
-         );
+	Port ( clk : in STD_LOGIC;
+		selector : out STD_LOGIC_VECTOR (1 downto 0);
+		segment : out STD_LOGIC_VECTOR (3 downto 0)
+	);
 end component;
 
 --Signals:
@@ -82,17 +84,28 @@ signal sig_n2_C      : STD_LOGIC_VECTOR(3 downto 0);
 signal sig_n3_D      : STD_LOGIC_VECTOR(3 downto 0);
 signal sig_out_hex   : STD_LOGIC_VECTOR(3 downto 0);
 signal sig_sel       : STD_LOGIC_VECTOR(1 downto 0);
-
+signal sig_data_rx   : STD_LOGIC_VECTOR(7 downto 0);
 signal sig_cntr      : INTEGER := 0;
-signal clk_50Hz      : STD_LOGIC := '0';
+signal sig_align     : STD_LOGIC_VECTOR(15 downto 0);
+signal sig_rx_data   : STD_LOGIC_VECTOR(7 downto 0);       
+signal sig_clk_trans : STD_LOGIC;
+signal sig_clk_disp  : STD_LOGIC;
+signal sig_rx        : STD_LOGIC := '1';
+
 begin
-     m_hex: led_segment
-        port map(sig_out_hex,display);
-     m_demux: demux
-        port map(sig_n0_A,sig_n1_B,sig_n2_C,sig_n3_D,sig_out_hex,sig_sel);
-     m_word2hex: word2hex
-        port map(word,sig_n0_A,sig_n1_B,sig_n2_C,sig_n3_D);
-     m_segment_driver: segment_driver
-        port map(clk,sig_sel,segments);
-        
+sig_rx <= rx;
+sig_clk_trans <= clk;
+sig_clk_disp <= clk;
+sig_align(7 downto 0) <= sig_rx_data;
+sig_align(15 downto 8) <= x"DA";
+
+	m_hex: led_segment
+		port map(sig_out_hex,display);
+	m_demux: demux
+		port map(sig_n0_A,sig_n1_B,sig_n2_C,sig_n3_D,sig_out_hex,sig_sel);
+	m_word2hex: word2hex
+		port map(sig_align,sig_n0_A,sig_n1_B,sig_n2_C,sig_n3_D);
+	m_segment_driver: segment_driver
+		port map(sig_clk_disp,sig_sel,segments);
+
 end behavior;
