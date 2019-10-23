fea1bb1c209fc3bbec23305b5f96fbd8511d6bfb
Dev4Embedded <integralzerox@gmail.com>
2019-10-23 20:05:26 +0200

    code cleaning and alignment: segment_driver.vhd

--- LedDisplay.srcs/sources_1/new/segment_driver.vhd
+++ LedDisplay.srcs/sources_1/new/segment_driver.vhd
@@ -33,9 +33,11 @@ use IEEE.numeric_std.ALL;
 --use UNISIM.VComponents.all;
 
 entity segment_driver is
-    Port ( clk : in STD_LOGIC;
-           selector : out STD_LOGIC_VECTOR (1 downto 0);
-           segment : out STD_LOGIC_VECTOR (3 downto 0));
+	Port ( 
+		clk : in STD_LOGIC;
+		selector : out STD_LOGIC_VECTOR (1 downto 0);
+		segment : out STD_LOGIC_VECTOR (3 downto 0)
+		);
 end segment_driver;
 
 architecture Behavioral of segment_driver is
@@ -43,29 +45,29 @@ signal result : STD_LOGIC_VECTOR(3 downto 0);
 signal cntr : UNSIGNED(1 downto 0) := (others=> '0');
 signal prescaler : UNSIGNED (31 downto 0) := (others => '0');
 begin
- 
-    segment <= result;
-    
-    counter_proc: process(clk)
-    begin
-        if(rising_edge(clk))then
-            prescaler <= prescaler + 1;
-            if(prescaler >= 500000)then
-                prescaler <= (others => '0');
-                selector <= STD_LOGIC_VECTOR(cntr);
-                case cntr is
-                    when "00" => result <= "1110";
-                    when "01" => result <= "1101";
-                    when "10" => result <= "1011";
-                    when "11" => result <= "0111"; 
-                    when others => result <= "1111";
-                end case;
-                cntr <= cntr + 1;
-                if(cntr > 3)then
-                    cntr <= "00";
-                end if;
-             end if;
-        end if;
-    end process counter_proc;
+
+	segment <= result;
+
+counter_proc: process(clk)
+	begin
+		if(rising_edge(clk))then
+			prescaler <= prescaler + 1;
+			if(prescaler >= 500000)then
+				prescaler <= (others => '0');
+				selector <= STD_LOGIC_VECTOR(cntr);
+				case cntr is
+					when "00" => result <= "1110";
+					when "01" => result <= "1101";
+					when "10" => result <= "1011";
+					when "11" => result <= "0111"; 
+					when others => result <= "1111";
+				end case;
+				cntr <= cntr + 1;
+				if(cntr > 3)then
+					cntr <= "00";
+				end if;
+			end if;
+		end if;
+	end process counter_proc;
 
 end Behavioral;
