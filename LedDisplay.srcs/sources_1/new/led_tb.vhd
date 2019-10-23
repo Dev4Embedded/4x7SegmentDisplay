b66e24edd55569a4680b480f292fb96121e7ee11
Dev4Embedded <integralzerox@gmail.com>
2019-10-23 20:03:27 +0200

    code cleaning and alignment: led_tb.vhd

--- LedDisplay.srcs/sources_1/new/led_tb.vhd
+++ LedDisplay.srcs/sources_1/new/led_tb.vhd
@@ -26,32 +26,31 @@ end;
 
 architecture bench of led_segment_tb is
 
-  component led_segment
-      Port (
-             hex : in STD_LOGIC_VECTOR (3 downto 0);
-             leds : out STD_LOGIC_VECTOR (6 downto 0)
-            );
-  end component;
+component led_segment
+	Port (
+		hex : in STD_LOGIC_VECTOR (3 downto 0);
+		leds : out STD_LOGIC_VECTOR (6 downto 0)
+		);
+end component;
 
-  signal hex: STD_LOGIC_VECTOR (3 downto 0);
-  signal leds: STD_LOGIC_VECTOR (6 downto 0) ;
+signal hex: STD_LOGIC_VECTOR (3 downto 0);
+signal leds: STD_LOGIC_VECTOR (6 downto 0) ;
 
 begin
 
-  uut: led_segment port map ( hex  => hex,
-                              leds => leds );
+uut: led_segment port map ( hex  => hex,
+						    leds => leds );
 
-  stimulus: process
-  begin
-    hex <= "0000";
- --   leds <= "0000000";
-    for i in 0 to 16 loop
-        hex <= STD_LOGIC_VECTOR(to_unsigned(i,4));
-        wait for 1 ns;
-    end loop;
+stimulus: process
+begin
+	hex <= "0000";
+--   leds <= "0000000";
+	for i in 0 to 16 loop
+		hex <= STD_LOGIC_VECTOR(to_unsigned(i,4));
+		wait for 1 ns;
+	end loop;
 
-    wait;
-  end process;
-
+	wait;
+end process;
 
 end;
